b64e927fb508d9998d668f8ebd63fa833a97e13d
Marie Goa <mgoa90334@gmail.com>
2023-02-24 12:06:13 +0000

    new

--- dossierjs/connexion.js
+++ dossierjs/connexion.js
@@ -7,9 +7,14 @@ btnCon.addEventListener('click', (e)=>{
     let nom = document.querySelector('.nom').value
     let adressemail = document.querySelector('.email').value
     let motdepasse = document.querySelector('.mdp').value
+    // let user = localStorage.getItem()
 
     if (nom === localStorage.getItem('nom') && adressemail === localStorage.getItem('email') && motdepasse === localStorage.getItem('mdp')){
-        alert("mo")
+
+        console.log("ok");
         location.pathname = '../dossierhtml/indexprofil.html'
+    }else{
+        alert("mot de passe ou identifiant incorrecte");
+        console.log("ok");
     }
 })
